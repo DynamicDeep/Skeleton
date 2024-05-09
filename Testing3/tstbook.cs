c9e84fd09a80fc3dee6dc06c6b9db27d4440ec7d
Ermilchantre <P2740050@my365.dmu.ac.uk>
2024-05-09 22:15:35 +0100

    This Is my first test for book management

--- Testing3/tstbook.cs
+++ Testing3/tstbook.cs
@@ -12,5 +12,43 @@ namespace Testing3
             clsbook Book = new clsbook();
             Assert.IsNotNull(Book);
         }
+        [TestMethod]
+        public void TitlePropertyOKk()
+        {
+            clsbook Book = new clsbook();
+            string TestData = "Romeu";
+            Book.Title = TestData;
+            
+            
+            Assert.AreEqual(Book.Title, TestData);
+
+        }
+        [TestMethod]
+        public void AuthorPropertyOK()
+        {
+            clsbook Book = new clsbook();
+            string TestData = "Shakespeare";
+            Book.Author = TestData;
+            Assert.AreEqual(Book.Author, TestData);
+      
+        }
+        [TestMethod]
+        public void PublicationyearOK()
+        {
+            clsbook Book = new clsbook();
+            DateTime TestData = DateTime.Now.Date;
+            Book.PublicationDate = TestData;
+            Assert.AreEqual(Book.PublicationDate, TestData);
+
+        }
+        [TestMethod]
+        public void PriceOK()
+        {
+            clsbook Book = new clsbook();
+            Decimal TestData = 0.99m;
+            Book.Price = TestData;
+            Assert.AreEqual(Book.Price, TestData);
+
+        }
     }
 }
