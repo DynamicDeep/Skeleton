c6b6f4e79b9f7f6545f4cc33310b01a46ce7fef1
Shaurya Bhatt <p2785739@m365.dmu.ac.uk>
2024-05-14 11:39:15 +0100

    Lab 7 all test

---
+++ ClassLibrary/clsStaff.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class clsStaff
+    {
+        public int StaffId { get; set; }
+        public int StaffPhoneNumber { get; set; }
+        public string StaffName { get; set; }
+        public string StaffAddress { get; set; }
+        public string StaffEmail { get; set; }
+        public int PositionId { get; set; }
+    }
+}
