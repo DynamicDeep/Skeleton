df13f14fc98f55b7a7cf8e1a33fb0dd7900d4aa7
Jagruti Pandya <P2596579@my365.dmu.ac.uk>
2024-05-14 13:29:50 +0100

    Completed Lab 6-7

---
+++ ClassLibrary/clsOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsOrder
+    {
+        public int OrderId { get; set; }
+        public int OrderID { get; set; }
+        public int CustomerID { get; set; }
+        public int CustomerId { get; set; }
+        public double TotalCost { get; set; }
+        public string OrderNotes { get; set; }
+        public string Address { get; set; }
+        public DateTime DeliveryDate { get; set; }
+    }
+}
