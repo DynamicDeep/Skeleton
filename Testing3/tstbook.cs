a618ab4aa18d16ccb3db343cdd6bac0726bf8fcb
Ermilchantre <P2740050@my365.dmu.ac.uk>
2024-05-21 21:35:53 +0100

    Updates

--- Testing3/tstbook.cs
+++ Testing3/tstbook.cs
@@ -33,7 +33,7 @@ namespace Testing3
             clsbook Book = new clsbook();
 
             // Crie alguns dados de teste para atribuir à propriedade
-            int TestData = 1; // Use int em vez de Int32
+            Int32 TestData = 25; // Use int em vez de Int32
 
             // Atribua os dados à propriedade
             Book.BookId = TestData;
@@ -89,7 +89,7 @@ namespace Testing3
         public void BookManagementPropertyoOK()
         {
             clsbook Book = new clsbook();
-            Decimal TestData = 0.99m;
+            string TestData = "Test data";
             Book.BookManagement = TestData;
             Assert.AreEqual(Book.BookManagement, TestData);
 
@@ -118,7 +118,7 @@ namespace Testing3
             clsGenres Genre = new clsGenres();
 
             // Crie alguns dados de teste para atribuir à propriedade
-            int TestData = 1; // Use int em vez de Int32
+            int TestData = 21; // Use int em vez de Int32
 
             // Atribua os dados à propriedade
             Genre.GenreId = TestData;
@@ -149,7 +149,7 @@ namespace Testing3
             clsGenres Genre = new clsGenres();
 
             // Crie alguns dados de teste para atribuir à propriedade
-            int TestData = 1; // Assume que BookId é do tipo int
+            Int32 TestData = 21; // Assume que BookId é do tipo int
 
             // Atribua os dados à propriedade
             Genre.BookId = TestData;
@@ -158,7 +158,330 @@ namespace Testing3
             Assert.AreEqual(Genre.BookId, TestData);
         }
 
+
+        //********************************************************************START THE FIND METOD GENRE*****************************************************************************
+
+        [TestMethod]
+        public void FindGenreIdMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsGenres Genre = new clsGenres();
+            //create a Boolean variable to store the results of the validation
+            Boolean Found = false;
+            //Creat boolean variable to record if the data is ok(assumi it id)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 GenreId = 21;
+            //invoke the method
+            Found = Genre.Find(GenreId);
+            //check the addess
+            Assert.IsTrue(Found);
+        }
+
+
+        [TestMethod]
+        public void FindGenreId2MethodOK()
+        {
+            //create an instance of the class we want to create
+            clsGenres Genre = new clsGenres();
+            //create a Boolean variable to store the results of the validation
+            Boolean Found = false;
+            //Creat boolean variable to record if the data is ok(assumi it id)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 GenreId = 21;
+            //invoke the method
+            Found = Genre.Find(GenreId);
+            //check the addess
+            if (Genre.GenreId != 21)
+
+            {
+                OK = false;
+            }
+            
+            Assert.IsTrue(Found);
+        }
+        [TestMethod]
+        public void FindGenrForigianKMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsGenres Genre = new clsGenres();
+            //create a Boolean variable to store the results of the validation
+            Boolean Found = false;
+            //Creat boolean variable to record if the data is ok(assumi it id)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 GenreId = 21;
+            //invoke the method
+            Found = Genre.Find(GenreId);
+            //check the addess
+            if (Genre.BookId != 21)
+
+            {
+                OK = false;
+            }
+
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void FindBookGenreMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsGenres Genre = new clsGenres();
+            //create a Boolean variable to store the results of the validation
+            Boolean Found = false;
+            //Creat boolean variable to record if the data is ok(assumi it id)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 GenreId = 21;
+            //invoke the method
+            Found = Genre.Find(GenreId);
+            //check the addess
+            if (Genre.BookGenre != "Romance")
+
+            {
+                OK = false;
+            }
+
+            Assert.IsTrue(Found);
+        }
+
+
+        //********************************************************FIND METOD Book*************************************************************************
+        [TestMethod]
+        public void FindMetBoodOK()
+        {
+            //create an instance of the class we want to create
+            clsbook Book = new clsbook();
+            //create a Boolean variable to store the results of the validation
+            Boolean Found = false;
+            //Creat boolean variable to record if the data is ok(assumi it id)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 BookId = 21;
+            //invoke the method
+            Found = Book.Find(BookId);
+            Assert.IsTrue(Found);
+
+        }
+        //********************************************************FIND METOD Book*************************************************************************
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsbook Book = new clsbook();
+            //create a Boolean variable to store the results of the validation
+            Boolean Found = false;
+            //Creat boolean variable to record if the data is ok(assumi it id)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 BookId = 21;
+            //invoke the method
+            Found = Book.Find(BookId);
+            if (Book.BookId != 21)
+            {
+                OK = false;
+            }
+            //test to see the result is correct
+            Assert.IsTrue(OK);
+
+        }
+
+
+        //********************************************************FIND METOD Book*************************************************************************
+        [TestMethod]
+        public void FindMetodTitleOK()
+        {
+            //create an instance of the class we want to create
+            clsbook Book = new clsbook();
+            //create a Boolean variable to store the results of the validation
+            Boolean Found = false;
+            //Creat boolean variable to record if the data is ok(assumi it id)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 BookId = 21;
+            //invoke the method
+            Found = Book.Find(BookId);
+            //check the addess
+            if (Book.Title != "Romeeu&Julheta")
+            {
+                OK = false;
+            }
+            //test to see the result is correct
+            Assert.IsTrue(OK);
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        [TestMethod]
+        public void FindMetodStafManagementOK()
+        {
+            //create an instance of the class we want to create
+            clsbook Book = new clsbook();
+            //create a Boolean variable to store the results of the validation
+            Boolean Found = false;
+            //Create a Boolean variable to record if the data is ok (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 BookId = 21;
+            //invoke the method
+            Found = Book.Find(BookId);
+            //check the author
+            if (Book.Author != "Shakespeare")
+            {
+                OK = false;
+            }
+            //test to see the result is correct
+            Assert.IsTrue(OK);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        [TestMethod]
+        public void FindMetodAuthorOK()
+        {
+            //create an instance of the class we want to create
+            clsbook Book = new clsbook();
+            //create a Boolean variable to store the results of the validation
+            Boolean Found = false;
+            //Create a Boolean variable to record if the data is ok (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 BookId = 21;
+            //invoke the method
+            Found = Book.Find(BookId);
+            //check the author
+            if (Book.Author != "Shakespeare")
+            {
+                OK = false;
+            }
+            //test to see the result is correct
+            Assert.IsTrue(OK);
+        }
+
+
+
+
+        [TestMethod]
+        public void FindPriceMetodOK()
+        {
+            //create an instance of the class we want to create
+            clsbook Book = new clsbook();
+            //create a Boolean variable to store the results of the validation
+            Boolean Found = false;
+            //Creat boolean variable to record if the data is ok(assumi it id)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 BookId = 21;
+            //invoke the method
+            Found = Book.Find(BookId);
+            //check the addess
+            if (Book.Price != 0.99m)
+            {
+                OK = false;
+            }
+            //test to see the result is correct
+            Assert.IsTrue(OK);
+
+
+        }
+
+        [TestMethod]
+        public void FindPublicationyearMetodOK()
+        {
+            //create an instance of the class we want to create
+            clsbook Book = new clsbook();
+            //create a Boolean variable to store the results of the validation
+            Boolean Found = false;
+            //Creat boolean variable to record if the data is ok(assumi it id)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 BookId = 21;
+            //invoke the method
+            Found = Book.Find(BookId);
+            //check the addess
+            if (Book.PublicationYear != Convert.ToDateTime("23/12/2022"))
+            {
+                OK = false;
+            }
+            //test to see the result is correct
+            Assert.IsTrue(OK);
+
+
+        }
+        [TestMethod]
+        public void FindDescriptionMetodOK()
+        {
+            //create an instance of the class we want to create
+            clsbook Book = new clsbook();
+            //create a Boolean variable to store the results of the validation
+            Boolean Found = false;
+            //Creat boolean variable to record if the data is ok(assumi it id)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 BookId = 21;
+            //invoke the method
+            Found = Book.Find(BookId);
+            //check the addess
+            if (Book.Description !="This is my description")
+            {
+                OK = false;
+            }
+            //test to see the result is correct
+            Assert.IsTrue(OK);
+
+
+        }
+
+        [TestMethod]
+        public void FindImagePathMetodOK()
+        {
+            //create an instance of the class we want to create
+            clsbook Book = new clsbook();
+            //create a Boolean variable to store the results of the validation
+            Boolean Found = false;
+            //Creat boolean variable to record if the data is ok(assumi it id)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 BookId = 21;
+            //invoke the method
+            Found = Book.Find(BookId);
+            //check the addess
+            if (Book.ImagePath != "/images/book1.jpg")
+            {
+                OK = false;
+            }
+            //test to see the result is correct
+            Assert.IsTrue(OK);
+
+
+        }
     }
-
-
 }
