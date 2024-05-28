8460c34f6fda39d6a2f767e858ca150cc5f867ba
P2785119 <P2785119@my365.dmu.ac.uk>
2024-05-28 16:15:00 +0100

    Lab 12 Done! It is not good as a presentation layer prospect but the functionally is working properly

--- AdminSystem/[Deep]List.aspx.cs
+++ AdminSystem/[Deep]List.aspx.cs
@@ -4,11 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (IsPostBack == false)
+        {
+            DisplayStocks();
+        }
+    }
+
+    void DisplayStocks()
+    {
+        clsStockCollection stock = new clsStockCollection();
+        lstStockList.DataSource = stock.StockList;
+        lstStockList.DataValueField = "stockId";
+        lstStockList.DataTextField = "BookId";
+        lstStockList.DataBind();    
 
     }
 }
