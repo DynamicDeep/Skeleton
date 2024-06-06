bef13866bc38b2b5916a3b94cc29a32b2bab5d37
Shaurya Bhatt <p2785739@m365.dmu.ac.uk>
2024-06-06 03:53:29 +0100

    Update Staff

--- AdminSystem/[Shaurya]List.aspx.cs
+++ AdminSystem/[Shaurya]List.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using ClassLibrary;
-using Testing1;
+
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
--- ClassLibrary/clsStaffCollection.cs
+++ ClassLibrary/clsStaffCollection.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Testing1
+namespace ClassLibrary
 {
     public class clsStaffCollection
     {
